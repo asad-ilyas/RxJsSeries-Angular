2bda200dae0a2ce557252032fb09a165d0ccc562
Hp <Hp@DESKTOP-NEERTGR>
2021-05-09 16:07:44 +0500

    promises completed

--- src/app/promise/promise.component.ts
+++ src/app/promise/promise.component.ts
@@ -7,16 +7,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PromiseComponent implements OnInit {
 
+  promiseValues: any;
+  dell = {
+    brand: 'Dell',
+    hardDisk: '2 TB SSG',
+    color: 'Black',
+    status: 'Success'
+  }
+  hp = {
+    brand: 'Hp',
+    hardDisk: '1 TB SSG',
+    color: 'Silver',
+    status: 'Success'
+  }
+  notAvailable = {
+    brand: 'Not Available',
+    status: 'Failed'
+  }
   constructor() { }
 
+  dellAvailable() {
+    return true;
+
+  }
+  hpAvailable() {
+    return false;
+  }
   ngOnInit(): void {
     let buyLaptop = new Promise((resolve, reject) => {
-      resolve('promise is resolved');
+      if (this.dellAvailable()) {
+        return setTimeout(() => {
+          // resolve('Dell is Available');
+          resolve(this.dell);
+        }, 3000);
+      } else if (this.hpAvailable()) {
+        return setTimeout(() => {
+          // resolve('Hp is Purchase');
+          resolve(this.hp);
+        }, 3000);
+      } else {
+        return setTimeout(() => {
+          // reject('Laptop is not available in the store');
+          reject(this.notAvailable);
+        }, 3000);
+      }
     });
     buyLaptop.then(res => {
-      console.log('success', res);
-
-    })
+      console.log('resolve =>', res);
+      this.promiseValues = res;
+    }).catch(err => {
+      console.log('reject =>', err);
+      this.promiseValues = err;
+    });
   }
-
 }
